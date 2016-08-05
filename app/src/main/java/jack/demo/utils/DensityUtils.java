de8fb60d22d603d3b8eb33cb5f1ec16b2029d70b
langya <pjwei1013@163.com>
2016-08-05 15:07:07 +0800

    添加基础工具类

--- app/src/main/java/jack/demo/utils/DensityUtils.java
+++ app/src/main/java/jack/demo/utils/DensityUtils.java
@@ -1,9 +1,10 @@
 package jack.demo.utils;
 
 import android.content.Context;
+import android.util.TypedValue;
 
 /**
- * Destriptions:
+ * Destriptions:单位转换类
  * Created by weipengjie on 16/8/2.
  */
 public class DensityUtils {
@@ -21,6 +22,7 @@ public class DensityUtils {
 
     /**
      * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     *
      * @param context
      * @param dpValue
      * @return
@@ -28,5 +30,28 @@ public class DensityUtils {
     public static int dip2px(Context context, float dpValue) {
         final float scale = context.getResources().getDisplayMetrics().density;
         return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * sp转px
+     *
+     * @param context
+     * @param spVal
+     * @return
+     */
+    public static int sp2px(Context context, float spVal) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP,
+                spVal, context.getResources().getDisplayMetrics());
+    }
+
+    /**
+     * px转sp
+     *
+     * @param context
+     * @param pxVal
+     * @return
+     */
+    public static float px2sp(Context context, float pxVal) {
+        return (pxVal / context.getResources().getDisplayMetrics().scaledDensity);
     }
 }
