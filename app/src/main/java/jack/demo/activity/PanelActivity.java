4e87deb0a57ce89f5a1f2f5bd105acec06040114
langya <pjwei1013@163.com>
2016-08-05 17:07:00 +0800

    *feature)添加自定义抽屉控件Panel

---
+++ app/src/main/java/jack/demo/activity/PanelActivity.java
@@ -0,0 +1,18 @@
+package jack.demo.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import jack.demo.R;
+
+/**
+ * Destriptions:
+ * Created by weipengjie on 16/8/5.
+ */
+public class PanelActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_panel_layout);
+    }
+}
