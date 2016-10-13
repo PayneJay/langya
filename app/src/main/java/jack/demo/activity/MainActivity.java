cb87f99d966ba617ada7feac63c1627292d358c3
weipengjie <weipengjie@fishtrip.cn>
2016-10-13 14:20:59 +0800

    *feature)删除冗余代码

--- app/src/main/java/jack/demo/activity/MainActivity.java
+++ app/src/main/java/jack/demo/activity/MainActivity.java
@@ -52,11 +52,7 @@ public class MainActivity extends Activity {
      */
     private void registerXGPush() {
         // 开启logcat输出，方便debug，发布时请关闭
-         XGPushConfig.enableDebug(this, true);
-        // 如果需要知道注册是否成功，请使用registerPush(getApplicationContext(), XGIOperateCallback)带callback版本
-        // 如果需要绑定账号，请使用registerPush(getApplicationContext(),account)版本
-        // 具体可参考详细的开发指南
-        // 传递的参数为ApplicationContext
+        XGPushConfig.enableDebug(this, true);
         Context context = getApplicationContext();
         XGPushManager.registerPush(context, new XGIOperateCallback() {
             @Override
@@ -69,17 +65,6 @@ public class MainActivity extends Activity {
                 Log.d("TPush", "注册失败，错误码：" + errCode + ",错误信息：" + msg);
             }
         });
-
-        // 2.36（不包括）之前的版本需要调用以下2行代码
-//        Intent service = new Intent(context, XGPushService.class);
-//        context.startService(service);
-
-        // 其它常用的API：
-        // 绑定账号（别名）注册：registerPush(context,account)或registerPush(context,account, XGIOperateCallback)，其中account为APP账号，可以为任意字符串（qq、openid或任意第三方），业务方一定要注意终端与后台保持一致。
-        // 取消绑定账号（别名）：registerPush(context,"*")，即account="*"为取消绑定，解绑后，该针对该账号的推送将失效
-        // 反注册（不再接收消息）：unregisterPush(context)
-        // 设置标签：setTag(context, tagName)
-        // 删除标签：deleteTag(context, tagName)
     }
 
     /**
@@ -113,8 +98,6 @@ public class MainActivity extends Activity {
             adapter.addAll(names);
 
             list.setAdapter(adapter);
-
-
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
         }
