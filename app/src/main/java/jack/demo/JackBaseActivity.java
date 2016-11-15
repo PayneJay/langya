dbb8d1b19b2f02fa88e6150c2e9698981d8e3cbb
weipengjie <weipengjie@fishtrip.cn>
2016-11-15 15:06:09 +0800

    *feature）添加Google新推出的EditText辅助控件TextInputLayout

--- app/src/main/java/jack/demo/JackBaseActivity.java
+++ app/src/main/java/jack/demo/JackBaseActivity.java
@@ -2,6 +2,10 @@ package jack.demo;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.tencent.android.tpush.XGPushClickedResult;
 import com.tencent.android.tpush.XGPushManager;
@@ -13,7 +17,41 @@ import jack.demo.activity.ProgressViewActivity;
  * Created by weipengjie on 2016/10/13.
  */
 
-public class JackBaseActivity extends Activity {
+public class JackBaseActivity extends Activity implements View.OnClickListener {
+    ImageView ivTopBack;
+    TextView tvTopTitle;
+    ImageView ivTopSave;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.base_layout);
+
+        initView();
+    }
+
+    private void initView() {
+        ivTopBack = (ImageView) findViewById(R.id.iv_top_back);
+        tvTopTitle = (TextView) findViewById(R.id.tv_top_title);
+        ivTopSave = (ImageView) findViewById(R.id.iv_top_save);
+        ivTopBack.setOnClickListener(this);
+        tvTopTitle.setOnClickListener(this);
+        ivTopSave.setOnClickListener(this);
+    }
+
+    /**
+     * 点击左上角返回
+     */
+    protected void topLeftBack() {
+        finish();
+    }
+
+    /**
+     * 点击右上角保存
+     */
+    protected void topRightSave() {
+
+    }
 
     @Override
     protected void onResume() {
@@ -34,5 +72,17 @@ public class JackBaseActivity extends Activity {
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
         setIntent(intent);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.iv_top_back:
+                topLeftBack();
+                break;
+            case R.id.iv_top_save:
+                topRightSave();
+                break;
+        }
     }
 }
